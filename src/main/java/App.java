e3e86783b400fed3148158a7e5e13936f62a656d
matt dowdney <theofficialmatt@gmail.com>
2018-05-27 12:09:21 -0700

    add form input and dynamic greeting card

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -12,6 +14,18 @@ public class App {
         }, new HandlebarsTemplateEngine());
         get("/favorite_photos", (request, response) -> {
             return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
